0cf55c4860d0f866a74decdc058a20abc9833224
timothy <timothy.waweru1@student.moringaschool.com>
2025-03-10 20:35:09 +0300

    God did

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+ 
+}
+saturdayFun ();
+function mondayWork (event = "go to the office") {
+    return `This Monday, I will ${event}.`;
+}
+mondayWork ();
+
+
+function wrapAdjective (mark = "*") {
+    return function (adj = "special" ) {
+        return `You are ${mark}${adj}${mark}!`
+        
+    }
+}
+
+
